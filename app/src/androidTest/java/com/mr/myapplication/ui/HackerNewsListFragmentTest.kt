9252fe543ecaa4c5f96efd6d647d46da16f09be2
mohammed.rampurawala <mohom.007@gmail.com>
2019-07-15 22:59:32 +0530

    - Espresso contrib and intents dependencies added.

--- app/src/androidTest/java/com/mr/myapplication/ui/HackerNewsListFragmentTest.kt
+++ app/src/androidTest/java/com/mr/myapplication/ui/HackerNewsListFragmentTest.kt
@@ -1,9 +1,19 @@
 package com.mr.myapplication.ui
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.core.internal.deps.guava.collect.Iterables
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
@@ -12,14 +22,13 @@ import com.mr.myapplication.TestHackerApplication
 import com.mr.myapplication.TestHackerComponent
 import com.mr.myapplication.network.Story
 import com.mr.myapplication.ui.home.HackerNewsListActivity
-import com.mr.myapplication.ui.home.HomeNewsListFragment
+import com.mr.myapplication.ui.news.NewsActivity
 import io.reactivex.Single
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito
-import java.lang.Exception
 import java.util.*
 import java.util.concurrent.TimeUnit
 
@@ -31,6 +40,9 @@ class HackerNewsListFragmentTest {
 
     @get:Rule
     val activityRule = ActivityTestRule<HackerNewsListActivity>(HackerNewsListActivity::class.java, false, false)
+
+    @get:Rule
+    val intentActivityRule = IntentsTestRule<HackerNewsListActivity>(HackerNewsListActivity::class.java, false, false)
 
     @get:Rule
     val rule = InstantTaskExecutorRule()
@@ -50,7 +62,8 @@ class HackerNewsListFragmentTest {
     @Test
     fun testLoadingIsDisplayed() {
         val hackerNewsApi = (app.hackerComponent as TestHackerComponent).getHackerNewsApi()
-        Mockito.`when`(hackerNewsApi.getTopStories()).thenReturn(Single.just(Arrays.asList(2000L)).delay(2,TimeUnit.SECONDS))
+        Mockito.`when`(hackerNewsApi.getTopStories())
+            .thenReturn(Single.just(Arrays.asList(2000L)).delay(2, TimeUnit.SECONDS))
         Mockito.`when`(hackerNewsApi.getData("2000")).thenReturn(
             Single.just(
                 Story(
@@ -82,8 +95,68 @@ class HackerNewsListFragmentTest {
         activityRule.finishActivity()
     }
 
-    fun getFragment(): HomeNewsListFragment? {
-        return activityRule.activity.supportFragmentManager.findFragmentById(R.id.home_fragment) as HomeNewsListFragment
+    @Test
+    fun testRetry() {
+        val hackerNewsApi = (app.hackerComponent as TestHackerComponent).getHackerNewsApi()
+        Mockito.`when`(hackerNewsApi.getTopStories()).thenReturn(Single.error(Exception("Mock Exception")))
+        activityRule.launchActivity(null)
+        Thread.sleep(2000L)
+        //Test on error "Retry" screen is displayed
+        onView(ViewMatchers.withId(R.id.retry_view_news_list)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Mockito.`when`(hackerNewsApi.getTopStories())
+            .thenReturn(Single.just(Arrays.asList(2000L)).delay(2, TimeUnit.SECONDS))
+        Mockito.`when`(hackerNewsApi.getData("2000")).thenReturn(
+            Single.just(
+                Story(
+                    "by",
+                    "2000",
+                    Collections.emptyList(),
+                    20,
+                    123123L,
+                    "Title",
+                    "story",
+                    "https://www.google.com"
+                )
+            )
+        )
+        //After click on retry
+        onView(ViewMatchers.withId(R.id.retry)).perform(ViewActions.click())
+        //List is displayed with news list
+        onView(ViewMatchers.withId(R.id.loading_progress_bar)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Thread.sleep(5000L)
+        onView(ViewMatchers.withId(R.id.news_list_container)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        activityRule.finishActivity()
     }
 
+    @Test
+    fun testNewsClick() {
+        val hackerNewsApi = (app.hackerComponent as TestHackerComponent).getHackerNewsApi()
+        Mockito.`when`(hackerNewsApi.getTopStories())
+            .thenReturn(Single.just(Arrays.asList(2000L)))
+        Mockito.`when`(hackerNewsApi.getData("2000")).thenReturn(
+            Single.just(
+                Story(
+                    "by",
+                    "2000",
+                    Collections.emptyList(),
+                    20,
+                    123123L,
+                    "Title",
+                    "story",
+                    "https://www.google.com"
+                )
+            )
+        )
+        intentActivityRule.launchActivity(null)
+        //List is displayed with news list
+        onView(ViewMatchers.withId(R.id.news_list_container)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Thread.sleep(5000L)
+        // First scroll to the position that needs to be matched and click on it.
+        val actionOnItemAtPosition = actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click())
+        onView(ViewMatchers.withId(R.id.news_list_recycler_view)).perform(actionOnItemAtPosition)
+
+        //Then news activity should be opened
+        intended(hasComponent(NewsActivity::class.java.name))
+        assert(Iterables.getOnlyElement(Intents.getIntents()).getBundleExtra("bundle").getParcelable<Story>("story") != null)
+    }
 }
