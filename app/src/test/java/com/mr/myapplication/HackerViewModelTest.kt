2f1865e8532e345c69a6745ab8eca91bcc424a4b
mohammed.rampurawala <mohom.007@gmail.com>
2019-07-16 23:52:38 +0530

    - JUnit tag added.

--- app/src/test/java/com/mr/myapplication/HackerViewModelTest.kt
+++ app/src/test/java/com/mr/myapplication/HackerViewModelTest.kt
@@ -12,12 +12,15 @@ import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 import java.lang.Exception
 import java.util.*
 
+@RunWith(JUnit4::class)
 class HackerViewModelTest {
 
     @Rule
